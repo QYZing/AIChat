bed187c65a885bb4547096d31256a056df36fb20
QYZing <2062656460@qq.com>
2025-03-21 13:43:49 +0800

    增加sql文件

--- user.sql
+++ user.sql
@@ -1,71 +1,43 @@
-create table user
+create table users
 (
-    id           bigint auto_increment
+    id           int auto_increment
         primary key,
-    username     varchar(256)                       null comment '用户名称',
-    userAccount  varchar(256)                       null comment '账号',
-    avatarUrl    varchar(1024)                      null comment '头像',
-    gender       tinyint                            null comment '性别',
-    userPassword varchar(512)                       not null comment '密码',
-    phone        varchar(128)                       null comment '电话',
-    email        varchar(512)                       null comment '邮箱',
-    profile      varchar(512)                       null comment '个人简介',
-    userStatus   int      default 0                 not null comment '状态',
-    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint  default 0                 not null comment '是否删除',
-    userRole     int      default 0                 not null comment '0 - 普通用户 1 - 管理员',
-    tags         varchar(1024)                      null comment '标签 json 列表 '
+    username     varchar(50)                         not null,
+    userPassword varchar(255)                        not null,
+    created_at   timestamp default CURRENT_TIMESTAMP null,
+    constraint username
+        unique (username),
+    constraint users_pk
+        unique (username)
 );
 
-
-create table team
+create table sessions
 (
-    id          bigint auto_increment primary key,
-    name        varchar(256)                       not null comment '队伍名称',
-    description varchar(1024)                      null comment '队伍描述',
-    maxNum      int      default 1                 not null comment '最大人数',
-    expireTime  datetime                           null comment '过期时间',
-    userId      bigint                             comment '用户id',
-    Status      int      default 0                 not null comment '0 - 公开 1 - 私有 2 加密',
-    password    varchar(512)                       null comment '密码',
-
-    createTime  datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime  datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete    tinyint  default 0                 not null comment '是否删除'
-) comment '队伍';
-
-
-create table user_team
-(
-    id          bigint auto_increment primary key,
-    userId      bigint                             comment '用户id（队长id）',
-    teamId      bigint                             comment '队伍id',
-    joinTime  datetime                       null  comment '更新时间',
-
-    createTime  datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime  datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete    tinyint  default 0                 not null comment '是否删除'
-) comment '用户队伍关系';
-
-
-create table tag
-(
-    id         bigint auto_increment
+    id             varchar(36)                         not null
         primary key,
-    tagName    varchar(256)                       null comment '标签名称',
-    userId     bigint                             null comment '用户id',
-    parentId   bigint                             null comment '父标签id',
-    isParent   tinyint                            null comment '0 - 不是 1 - 父标签',
-    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
-    constraint uni_idx_tagName
-        unique (tagName)
-)
-    comment '标签';
+    user_id        int                                 not null,
+    assistant_type varchar(20)                         not null,
+    created_at     timestamp default CURRENT_TIMESTAMP null,
+    constraint sessions_ibfk_1
+        foreign key (user_id) references users (id)
+);
 
-create index idx_userId
-    on tag (userId);
+create table messages
+(
+    id         int auto_increment
+        primary key,
+    session_id varchar(36)                          not null,
+    content    text                                 not null,
+    role       enum ('user', 'assistant')           not null,
+    created_at timestamp  default CURRENT_TIMESTAMP null,
+    is_current tinyint(1) default 1                 null,
+    is_temp    int        default 0                 null,
+    constraint messages_pk
+        unique (session_id, role),
+    constraint messages_ibfk_1
+        foreign key (session_id) references sessions (id)
+);
 
+create index user_id
+    on sessions (user_id);
 
